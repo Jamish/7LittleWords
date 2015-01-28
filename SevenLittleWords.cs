65edffc4d8f962b6b04311c792e79d5e6014b1a8
Jamish <jamison.schuster@gmail.com>
2015-01-27 21:22:13 -0600

    Namespacing.

--- SevenLittleWords.cs
+++ SevenLittleWords.cs
@@ -3,96 +3,99 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-class SevenLittleWords
+namespace _7LittleWords
 {
-    private List<string> dictionary;
-    private List<string> _tiles;
-    public List<string> tiles
+    class SevenLittleWords
     {
-        get
+        private List<string> dictionary;
+        private List<string> _tiles;
+        public List<string> tiles
         {
-            return _tiles;
-        }
-        set
-        {
-            _tiles = value.Select(x => x.ToLower()).ToList();
-        }
-    }
-
-    public SevenLittleWords()
-    {
-        dictionary = File.ReadAllText("words.txt").Split(new string[] { Environment.NewLine }, System.StringSplitOptions.None).ToList();
-    }
-
-    public List<string> Solve(int length)
-    {
-        var potentialWords = new List<string>();
-        potentialWords = Recurse("", tiles, length);
-        var realWords = potentialWords.Intersect(dictionary);
-
-        return realWords.ToList();
-
-    }
-
-    public void EnterSolution(string word)
-    {
-        tiles = tiles.Except(TrimTiles(word)).ToList();
-    }
-
-    private List<string> Recurse(string word, List<string> remainingTiles, int length)
-    {
-        word = word.ToLower();
-        var results = new List<string>();
-
-        if (word.Length < length)
-        {
-            //Word has not reached the desired length, so recurse.
-            foreach (string tile in remainingTiles)
+            get
             {
-                var tiles = new List<string>(remainingTiles);
-                tiles.Remove(tile);
-                results.AddRange(Recurse(word + tile, tiles, length));
+                return _tiles;
+            }
+            set
+            {
+                _tiles = value.Select(x => x.ToLower()).ToList();
             }
         }
-        else if (word.Length == length)
+
+        public SevenLittleWords()
         {
-            //Word matches the right length; return it.
-            results.Add(word);
+            dictionary = File.ReadAllText("words.txt").Split(new string[] { Environment.NewLine }, System.StringSplitOptions.None).ToList();
         }
 
-        return results;
-    }
-
-    // Takes in a word and list of choices and returns the choices that were used to build the word.
-    private List<string> TrimTiles(string word)
-    {
-        var tilesToRemove = new List<string>();
-        word = word.ToLower();
-
-        bool noMatch = false;
-        while (word != "" && !noMatch)
+        public List<string> Solve(int length)
         {
-            noMatch = true; //Emergency check
-            foreach (string tile in tiles)
+            var potentialWords = new List<string>();
+            potentialWords = Recurse("", tiles, length);
+            var realWords = potentialWords.Intersect(dictionary);
+
+            return realWords.ToList();
+
+        }
+
+        public void EnterSolution(string word)
+        {
+            tiles = tiles.Except(TrimTiles(word)).ToList();
+        }
+
+        private List<string> Recurse(string word, List<string> remainingTiles, int length)
+        {
+            word = word.ToLower();
+            var results = new List<string>();
+
+            if (word.Length < length)
             {
-                if (word.StartsWith(tile))
+                //Word has not reached the desired length, so recurse.
+                foreach (string tile in remainingTiles)
                 {
-                    noMatch = false;
-                    word = word.Replace(tile, "");
-                    tilesToRemove.Add(tile);
-                    continue;
+                    var tiles = new List<string>(remainingTiles);
+                    tiles.Remove(tile);
+                    results.AddRange(Recurse(word + tile, tiles, length));
                 }
             }
+            else if (word.Length == length)
+            {
+                //Word matches the right length; return it.
+                results.Add(word);
+            }
+
+            return results;
         }
 
-        if (noMatch)
+        // Takes in a word and list of choices and returns the choices that were used to build the word.
+        private List<string> TrimTiles(string word)
         {
-            //Something broke... return empty. Should be null?
-            return new List<string>();
-        }
-        else
-        {
-            return tilesToRemove;
+            var tilesToRemove = new List<string>();
+            word = word.ToLower();
+
+            bool noMatch = false;
+            while (word != "" && !noMatch)
+            {
+                noMatch = true; //Emergency check
+                foreach (string tile in tiles)
+                {
+                    if (word.StartsWith(tile))
+                    {
+                        noMatch = false;
+                        word = word.Replace(tile, "");
+                        tilesToRemove.Add(tile);
+                        continue;
+                    }
+                }
+            }
+
+            if (noMatch)
+            {
+                //Something broke... return empty. Should be null?
+                return new List<string>();
+            }
+            else
+            {
+                return tilesToRemove;
+            }
         }
     }
 }
